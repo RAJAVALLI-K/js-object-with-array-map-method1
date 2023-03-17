2adf4c1fb8ca731e9c593cba48fb6365f991dc23
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-17 22:16:02 +0530

    add js-object with array map method1

---
+++ index.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+      first_name: 'Mike',
+      last_name: 'Sheridan',
+      age: 30
+    },
+    {
+      first_name: 'Tim',
+      last_name: 'Lee',
+      age: 45
+    },
+    {
+      first_name: 'John',
+      last_name: 'Carte',
+      age: 25
+    }
+  ];
+let result=users.map(function(value){
+  return value.last_name;
+});
+    console.log(result);
